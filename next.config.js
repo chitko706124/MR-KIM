30b6eb3882765a11bb95033ed765a65c4b17b5ed
chitko706124 <113786532+chitko706124@users.noreply.github.com>
2025-10-16 17:55:33 +0630

    Update next.config.js 2

--- next.config.js
+++ next.config.js
@@ -1,11 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  experimental: {
-    serverComponentsExternalPackages: ['@supabase/supabase-js']
+  trailingSlash: true,
+  async headers() {
+    return [
+      {
+        source: '/offers/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, max-age=0, must-revalidate',
+          },
+          {
+            key: 'CDN-Cache-Control',
+            value: 'no-cache'
+          },
+          {
+            key: 'Vercel-CDN-Cache-Control',
+            value: 'no-cache'
+          }
+        ],
+      },
+    ];
   },
 };
 
