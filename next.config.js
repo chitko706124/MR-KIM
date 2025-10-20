949591e31e4241d1d42ec8b2153b2f55285e2e0d
chitko706124 <113786532+chitko706124@users.noreply.github.com>
2025-10-20 17:18:15 +0630

    Update next.config.js 1

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // REMOVE output: 'export' completely
-  experimental: {
-    optimizeCss: false, // Disable CSS optimization
-  },
-  // Or disable preloading entirely
-  compress: false,
   
   eslint: {
     ignoreDuringBuilds: true,
